0e03ebbb0fd875040219a10e93ef59d27d009227
Zekiye Erdem <156846121+zekiyerdem@users.noreply.github.com>
2024-09-18 12:01:02 -0400

    Question 1 homework_3.sql
    I have completed the question 1

--- 02_activities/homework/homework_3.sql
+++ 02_activities/homework/homework_3.sql
@@ -3,6 +3,16 @@
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
 
+SELECT v.vendor_id, v.vendor_name, COUNT(vba.booth_number) AS booth_count
+FROM vendor v
+JOIN vendor_booth_assignments vba ON v.vendor_id = vba.vendor_id
+GROUP BY v.vendor_id, v.vendor_name;
+
+
+--**Explanation**
+-- that will join the vendor and vendor_booth_assignment table so that we can see the vendor's names, normally we can just use vendor_booth_assignment table without a join for that
+-- but this time we can't see the vendor's name so this way we can see the vendor's name
+
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -10,7 +20,19 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
+select c.customer_id, c.customer_last_name, c.customer_first_name, 
+sum(cp.cost_to_customer_per_qty*cp.quantity) as total_spent
+from customer c
+join customer_purchases cp on c.customer_id=cp.customer_id
+group by c.customer_id,c.customer_last_name,c.customer_first_name
+having total_spent>2000
+order by c.customer_last_name, c.customer_first_name;
 
+--*Explanation
+-- I have join customer and customer purchases table to be able see the customer last name and first name
+-- we need to use the having to filter the spent that is more than $2000
+-- Also I have created a new column that will calculate the total spent by multipling cost to customer and total quantity they have purchase
+-- lastly we order by last name and then first name which is asked in the question, I also add the customer is in the filtered table to distungish the cusotmer better if there is anyone has same name and surname
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
